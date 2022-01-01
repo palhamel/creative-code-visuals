09e97c636598c82dc1f64f988e96b2e575054661
Pål Hamel <pal.hamel@gmail.com>
2022-01-01 19:02:43 +0100

    add const for scalabel dimensions

--- sketches/sketch-01.js
+++ sketches/sketch-01.js
@@ -1,29 +1,35 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  // dimensions: [ 600, 600 ]
-  dimensions: 'A4',
-  pixelsPerInch: 300,
+  dimensions: [ 1080, 1080 ]
+  // dimensions: 'A4',
+  // pixelsPerInch: 300,
   
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = '#D2FEC6';
     context.fillRect(0, 0, width, height);
+    context.lineWidth = width * 0.01;
 
+    // Set for % of canvas dimension
+    const w = width * 0.10
+    const h = height * 0.10
+    const gap = width * 0.03
+    const ix =  width * 0.17
+    const iy = height * 0.17
 
-    const w = 60
-    const h = 60
-    const gap = 20
+    const offset = width * 0.02
+
     let x, y
 
     // Draw patterns with a loop:
     for (let i = 0; i < 5; i++) {
       // more rows:
       for (let j = 0; j < 5; j++) {
-        x = 100 + (w + gap) * i
-        y = 100 + (h + gap) * j
+        x = ix + (w + gap) * i
+        y = ix + (h + gap) * j
 
         context.beginPath()
         context.rect(x, y, w, h)
@@ -33,7 +39,7 @@ const sketch = () => {
         // Small square conditional drawing:
         if ( Math.random() > 0.5 ) {
           context.beginPath()
-          context.rect(x + 8, y + 8, w - 16, h - 16)
+          context.rect(x + offset / 2, y + offset / 2, w - offset, h - offset)
           context.strokeStyle = '#1B5FE8'
           context.stroke()
         }
