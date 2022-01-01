16bba6698cbcad0e3a55a5d37647d488b60f8aa7
Pål Hamel <pal.hamel@gmail.com>
2022-01-01 16:06:09 +0100

    add settings

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 ### Labs making graphics with code - work in progress 
+
+
+- run canasSketch via command line:
+
+# Run and open in browser:
+canvas-sketch src/foobar.js --open
+
+# Run the development server on an existing file
+canvas-sketch src/foobar.js
+
+# Start a new sketch from the Three.js template
+canvas-sketch --new --template=three --open
+
+# Build your sketch to a sharable HTML + JS website
+canvas-sketch sketches/my-sketch.js --build
+
+# Paste the clipboard contents & run a new sketch at './foo.js'
+pbpaste | canvas-sketch foo.js --new
+
+
+
+https://github.com/mattdesl/canvas-sketch
+
+https://github.com/mattdesl/canvas-sketch/blob/master/docs/README.md
+
--- sketches/sketch-01.js
+++ sketches/sketch-01.js
@@ -1,13 +1,50 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  // dimensions: [ 600, 600 ]
+  dimensions: 'A4',
+  pixelsPerInch: 300,
+  
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'green';
+    context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+
+    const w = 60
+    const h = 60
+    const gap = 20
+    let x, y
+
+    // Draw patterns with a loop:
+    for (let i = 0; i < 5; i++) {
+      // more rows:
+      for (let j = 0; j < 5; j++) {
+        x = 100 + (w + gap) * i
+        y = 100 + (h + gap) * j
+
+        context.beginPath()
+        context.rect(x, y, w, h)
+        context.strokeStyle = '#E81BB6'
+        context.stroke()
+
+        // Small square conditional drawing:
+        if ( Math.random() > 0.5 ) {
+          context.beginPath()
+          context.rect(x + 8, y + 8, w - 16, h - 16)
+          context.strokeStyle = '#1B5FE8'
+          context.stroke()
+        }
+        // if (i > 0 && i < 4) {
+        //   context.beginPath()
+        //   context.rect(x + 8, y + 8, w - 16, h - 16)
+        //   context.strokeStyle = '#E87B1B'
+        //   context.stroke()
+        // }
+      }
+    } 
   };
 };
 
