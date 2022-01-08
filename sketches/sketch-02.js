8d5e5d8a4d85ae110e30fecc5e572877158dabbf
Pål Hamel <pal.hamel@gmail.com>
2022-01-08 20:26:53 +0100

    add utility functions, ES6 imports

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -1,18 +1,23 @@
-const canvasSketch = require('canvas-sketch')
+import canvasSketch from 'canvas-sketch'
+import { degToRad } from 'canvas-sketch-util/math'
+import { range } from 'canvas-sketch-util/random'
+// const canvasSketch = require('canvas-sketch')
+// const math = require('canvas-sketch-util/math')
+// const random = require('canvas-sketch-util/random')
 
 const settings = {
   dimensions: [1080, 1080],
 }
 
 // Function for degrees:
-const degToRad = (degrees) => {
-  return (degrees / 180) * Math.PI
-}
+// const degToRad = (degrees) => {
+//   return (degrees / 180) * Math.PI
+// }
 
 // Function for random range:
-const randomRange = (min, max) => {
-  return Math.random() * (max - min) + min
-}
+// const randomRange = (min, max) => {
+//   return Math.random() * (max - min) + min
+// }
 
 const sketch = () => {
   return ({ context, width, height }) => {
@@ -45,7 +50,7 @@ const sketch = () => {
       context.save()
       context.translate(x, y)
       context.rotate(- angle)
-      context.scale(randomRange(1, 3), 1)
+      context.scale(range(1, 3), 1)
   
       context.beginPath()
       context.rect(-w * 0.5, -h * 0.5, w, h)
